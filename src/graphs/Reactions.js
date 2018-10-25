29b3494f2188a16c3cdda736c7893c1834942161
Alexey Kirpichnikov <alex.kirp@gmail.com>
2018-10-25 16:50:44 +0500

    Make reaction chart prettier

--- src/graphs/Reactions.js
+++ src/graphs/Reactions.js
@@ -1,12 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer, Bar} from "recharts";
+import {
+    Radar,
+    RadarChart,
+    PolarGrid,
+    PolarAngleAxis,
+    PolarRadiusAxis,
+    ResponsiveContainer
+} from "recharts";
 
 export default class Reactions extends React.Component {
     render () {
         return (
-            <ResponsiveContainer width="100%" height={200}>
-                <RadarChart data={convertReactionStats(this.props.data)}>
+            <ResponsiveContainer width="100%" height={300}>
+                <RadarChart data={convertReactionStats(this.props.data)} fontFamily="Roboto">
                     <PolarGrid />
                     <PolarAngleAxis dataKey="name" />
                     <PolarRadiusAxis/>
@@ -18,12 +25,9 @@ export default class Reactions extends React.Component {
 }
 
 Reactions.propTypes = {
-    data: PropTypes.arrayOf(PropTypes.shape({
-        name: PropTypes.string,
-        count: PropTypes.number,
-    }))
+    data: PropTypes.object
 };
 
 function convertReactionStats(moiraStats) {
-    return Object.keys(moiraStats).map(item => ({name: item, count: moiraStats[item]}))
+    return Object.keys(moiraStats).map(item => ({name: item, count: moiraStats[item]})).sort((a, b) => a.count > b.count ? -1 : 1)
 }
